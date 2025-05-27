db4a7345ce0f1bd246a838547587cac1bcb38228
Jalal Ali <alikhanumar795@gmail.com>
2025-05-27 22:41:33 +0500

    routes edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 import plugin from 'tailwindcss/plugin';
-import lineClamp from '@tailwindcss/line-clamp';
+// import lineClamp from '@tailwindcss/line-clamp';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -41,7 +41,7 @@ export default {
     },
   },
   plugins: [
-    lineClamp,
+    // lineClamp,
     plugin(function ({ addVariant, e, postcss }) {
       addVariant('firefox', ({ container, separator }) => {
         const isFirefoxRule = postcss.atRule({
