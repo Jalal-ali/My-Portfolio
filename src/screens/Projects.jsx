71c3cf470e5d30457e392503ff13063e6425729d
Jalal Ali <alikhanumar795@gmail.com>
2025-12-08 20:35:48 +0500

    updated

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -13,8 +13,8 @@ import Card from '../components/Card.jsx'
 import Madic from '../assets/projects/Madic.png'
 import Portfolio from '../assets/projects/portfolio.png'
 import BetterDay from '../assets/projects/betterDay.png'
-import SearchGurbani from '../assets/projects/searchGurbani.png'
-import MadAlpha from '../assets/projects/madAlpha.png'
+// import SearchGurbani from '../assets/projects/searchGurbani.png'
+// import MadAlpha from '../assets/projects/madAlpha.png'
 
 const Projects = () => {
     const projects = [
